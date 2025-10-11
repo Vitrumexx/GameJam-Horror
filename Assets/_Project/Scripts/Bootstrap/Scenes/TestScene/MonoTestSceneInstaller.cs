b36284fc086ac4135b6dc7b68646269e729029fd
Usevalad Buben <usevaladbuben@gmail.com>
2025-10-11 23:56:03 +0300

    Add MonoTestSceneInstaller for TestScene setup
    Introduces MonoTestSceneInstaller as a Zenject MonoInstaller for the TestScene. This provides a dedicated place to configure dependency injection bindings for the scene.

---
+++ Assets/_Project/Scripts/Bootstrap/Scenes/TestScene/MonoTestSceneInstaller.cs
@@ -0,0 +1,12 @@
+using Zenject;
+
+namespace _Project.Scripts.Bootstrap.Scenes.TestScene
+{
+    public class MonoTestSceneInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+            
+        }
+    }
+}
