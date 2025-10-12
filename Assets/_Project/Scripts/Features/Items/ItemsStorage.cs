e6ac277d82e49c32ac19cb0b4cedfceb61cd97aa
Usevalad Buben <usevaladbuben@gmail.com>
2025-10-13 02:08:17 +0300

    Add TryGetSoundStorableUnit method to ItemsStorage
    Introduces a method to safely retrieve an ItemStorableUnit by itemId, logging an error if the item is not found.

--- Assets/_Project/Scripts/Features/Items/ItemsStorage.cs
+++ Assets/_Project/Scripts/Features/Items/ItemsStorage.cs
@@ -1,8 +1,19 @@
 using _Project.Scripts.Features.Shared;
+using UnityEngine;
 
 namespace _Project.Scripts.Features.Items
 {
     public class ItemsStorage : MonoStorage<ItemStorableUnit>
     {
+        public bool TryGetSoundStorableUnit(string itemId, out ItemStorableUnit storableUnit)
+        {
+            if (!Units.TryGetValue(itemId, out storableUnit))
+            {
+                Debug.LogError($"Item ID {itemId} not found!");
+                return false;
+            }
+            
+            return true;
+        }
     }
 }
