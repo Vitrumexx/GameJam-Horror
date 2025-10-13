3567e14f5091bf56abe27c7b008edd6a5cb17b46
Usevalad Buben <usevaladbuben@gmail.com>
2025-10-13 03:00:40 +0300

    Add PlayerNotifier component for player notifications
    Introduces the PlayerNotifier MonoBehaviour with a NotifyPlayer method for displaying notifications to the player. This sets up the structure for future implementation of in-game notification logic.

---
+++ Assets/_Project/Scripts/Features/Player/PlayerNotifier.cs
@@ -0,0 +1,14 @@
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace _Project.Scripts.Features.Player
+{
+    [InfoBox("Оповещения для игрока (всплывающие уведомления)")]
+    public class PlayerNotifier: MonoBehaviour
+    {
+        public void NotifyPlayer(string msg)
+        {
+            
+        }
+    }
+}
