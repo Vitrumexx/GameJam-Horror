6f9b622b30ceef7092b9c4d756ab84f5a3cf631d
Usevalad Buben <usevaladbuben@gmail.com>
2025-10-13 03:00:52 +0300

    Add pickup and drop logic to Item class
    Introduced Drop and PickUp methods to manage item state, parenting, collider, and gravity. Added IsDropped property and improved initialization with warnings for missing IDs. Also enforced Collider requirement on the Item component.

--- Assets/_Project/Scripts/Features/Items/Item.cs
+++ Assets/_Project/Scripts/Features/Items/Item.cs
@@ -4,16 +4,41 @@ using UnityEngine;
 namespace _Project.Scripts.Features.Items
 {
     [RequireComponent(typeof(Rigidbody))]
+    [RequireComponent(typeof(Collider))]
     public class Item : MonoBehaviour
     {
-        public string id;
-        public Rigidbody Rigidbody { get; private set; }
-        public Collider Collider { get; private set; }
+        public string id = string.Empty;
+        private Rigidbody _rigidbody;
+        private Collider _collider;
+        public bool IsDropped { get; set; } = true;
 
         public void Start()
         {
-            Rigidbody = GetComponent<Rigidbody>();
-            Collider = GetComponent<Collider>();
+            if (id == string.Empty)
+            {
+                Debug.LogWarning($"No item {gameObject.name} ID specified!");
+            }
+            
+            _rigidbody = GetComponent<Rigidbody>();
+            _collider = GetComponent<Collider>();
+        }
+
+        public void Drop(Transform droppedStorage)
+        {
+            IsDropped = true;
+            
+            transform.SetParent(droppedStorage);
+            _collider.enabled = true;
+            _rigidbody.useGravity = true;
+        }
+
+        public void PickUp(Transform pickUpStorage)
+        {
+            IsDropped = false;
+            
+            transform.SetParent(pickUpStorage);
+            _collider.enabled = false;
+            _rigidbody.useGravity = false;
         }
     }
 }
