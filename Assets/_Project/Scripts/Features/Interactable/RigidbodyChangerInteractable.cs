ea93657a3eb76c949c10c8ab0deda13a493f2b8b
Usevalad Buben <usevaladbuben@gmail.com>
2025-10-15 01:40:39 +0300

    Add RigidbodyChangerInteractable script
    Introduces a new Interactable component that allows toggling the Rigidbody's isKinematic property when interacted with. Includes required component attributes and initialization logic.

---
+++ Assets/_Project/Scripts/Features/Interactable/RigidbodyChangerInteractable.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+namespace _Project.Scripts.Features.Interactable
+{
+    [RequireComponent(typeof(Rigidbody), typeof(Collider))]
+    public class RigidbodyChangerInteractable : Interactable
+    {
+        [Header("Conditions")]
+        
+        [Header("Changes on interact")]
+        public bool isKinematic = false;
+
+        private Rigidbody _rigidbody;
+        
+        protected override void Start()
+        {
+            base.Start();
+            
+            _rigidbody = GetComponent<Rigidbody>();
+        }
+        
+        public override void Interact()
+        {
+            _rigidbody.isKinematic = isKinematic;
+        }
+    }
+}
