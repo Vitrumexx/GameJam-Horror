bad23df485891a333bdea75e78cc6708e1f8a040
Usevalad Buben <usevaladbuben@gmail.com>
2025-10-14 22:57:23 +0300

    Add Key and ThrowableWeapon interactable item scripts
    Introduced new classes Key and ThrowableWeapon, both inheriting from InteractableItem. These scripts provide the structure for key and throwable weapon interactions, with TODOs for implementing door unlocking and enemy damage logic.

---
+++ Assets/_Project/Scripts/Features/Items/InteractableItems/Key.cs
@@ -0,0 +1,10 @@
+namespace _Project.Scripts.Features.Items.InteractableItems
+{
+    public class Key : InteractableItem
+    {
+        public override void Interact()
+        {
+            // TODO: find in given radius doors to open
+        }
+    }
+}
---
+++ Assets/_Project/Scripts/Features/Items/InteractableItems/ThrowableWeapon.cs
@@ -0,0 +1,10 @@
+namespace _Project.Scripts.Features.Items.InteractableItems
+{
+    public class ThrowableWeapon : InteractableItem
+    {
+        public override void Interact()
+        {
+            // TODO: add throw it and damage enemy
+        }
+    }
+}
