4ea0844e9fb4a1587e8f677183c3656d96be9754
Usevalad Buben <usevaladbuben@gmail.com>
2025-10-16 02:03:49 +0300

    Add itemPrefab field to ItemStorableUnit
    Introduced a public GameObject field 'itemPrefab' to the ItemStorableUnit class to allow assignment of a prefab associated with the item.

--- Assets/_Project/Scripts/Features/Items/ItemStorableUnit.cs
+++ Assets/_Project/Scripts/Features/Items/ItemStorableUnit.cs
@@ -5,6 +5,7 @@ namespace _Project.Scripts.Features.Items
 {
     public class ItemStorableUnit : StorableUnit
     {
+        public GameObject itemPrefab;
         public Sprite icon;
         public ItemWeight itemWeight = ItemWeight.Light;
         public string soundId;
