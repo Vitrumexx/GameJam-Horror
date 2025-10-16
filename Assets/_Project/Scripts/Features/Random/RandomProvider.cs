53ed745e7c7ce34e04c36a27899c0192c053d3f0
Usevalad Buben <usevaladbuben@gmail.com>
2025-10-16 05:42:26 +0300

    Add random item and rotation utilities to RandomProvider
    Introduced RandomRotationAxis struct and methods for generating random items from collections, as well as random Vector3 and Quaternion rotations based on specified axes. These additions enhance the utility of RandomProvider for randomization tasks.

--- Assets/_Project/Scripts/Features/Random/RandomProvider.cs
+++ Assets/_Project/Scripts/Features/Random/RandomProvider.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace _Project.Scripts.Features.Random
@@ -6,6 +8,21 @@ namespace _Project.Scripts.Features.Random
     public class RandomProvider : MonoBehaviour
     {
         public int seed;
+        
+        [Serializable]
+        public struct RandomRotationAxis
+        {
+            public RandomRotationAxis(bool x, bool y, bool z)
+            {
+                this.x = x;
+                this.y = y;
+                this.z = z;
+            }
+            
+            public bool x;
+            public bool y;
+            public bool z;
+        }
         
         private System.Random _random;
 
@@ -44,6 +61,27 @@ namespace _Project.Scripts.Features.Random
             if (collection == null || collection.Count == 0) return -1;
         
             return _random.Next(0, collection.Count);
+        }
+
+        public T Item<T>(ICollection<T> collection)
+        {
+            var randIndex = Index(collection);
+            return randIndex == -1 ? default : collection.ElementAt(randIndex);
+        }
+        
+        public Vector3 Rotation(RandomRotationAxis axis)
+        {
+            float x = axis.x ? _random.Next(0, 360) : 0;
+            float y = axis.y ? _random.Next(0, 360) : 0;
+            float z = axis.z ? _random.Next(0, 360) : 0;
+    
+            return new Vector3(x, y, z);
+        }
+
+        public Quaternion Quaternion(RandomRotationAxis axis)
+        {
+            var rotation = Rotation(axis);
+            return UnityEngine.Quaternion.Euler(rotation.x, rotation.y, rotation.z);
         }
     }
 }
